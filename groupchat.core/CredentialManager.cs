b31371f95d953df8bc26fe8d0caccf9712e80276
zemendaniel <zemen.daniel_2021c@bolyai-szakkozep.hu>
2025-09-22 19:57:35 +0200

    minor improvements

--- groupchat.core/CredentialManager.cs
+++ groupchat.core/CredentialManager.cs
@@ -1,19 +1,21 @@
-﻿using System.Security.Cryptography;
+﻿using System.Runtime.InteropServices;
+using System.Security.Cryptography;
 using System.Text;
 
 namespace groupchat.core;
 
-// todo multi platform
 public static class DPAPIHelper
 {
     public static byte[] Protect(string plaintext)
     {
+        if (!RuntimeInformation.IsOSPlatform(OSPlatform.Windows)) return [];
         var data = Encoding.UTF8.GetBytes(plaintext);
         return ProtectedData.Protect(data, null, DataProtectionScope.CurrentUser);
     }
 
     public static string Unprotect(byte[] encryptedData)
     {
+        if (!RuntimeInformation.IsOSPlatform(OSPlatform.Windows)) return "";
         var decrypted = ProtectedData.Unprotect(encryptedData, null, DataProtectionScope.CurrentUser);
         return Encoding.UTF8.GetString(decrypted);
     }
